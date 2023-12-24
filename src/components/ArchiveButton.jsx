9131262c9b6210a81e61bf2136bb0cf2092fbbf2
Alif Pahreza <agzmv123@gmail.com>
2023-12-24 23:51:44 +0800

    remove isArchive props in ArchiveButton

--- src/components/ArchiveButton.jsx
+++ src/components/ArchiveButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ArchiveButton = ({ id, onArchive, isArchive, children }) => {
-	return (
-		<button className='note-item__archive-button' onClick={() => onArchive(id)}>
-			{children}
-		</button>
-	);
+const ArchiveButton = ({ id, onArchive, children }) => {
+  return (
+    <button className='note-item__archive-button' onClick={() => onArchive(id)}>
+      {children}
+    </button>
+  );
 };
 
 export default ArchiveButton;
