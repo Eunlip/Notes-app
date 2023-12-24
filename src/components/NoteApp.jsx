494f02588d9ea3aa526a42f39c73d28f40b5ad30
Alif Pahreza <agzmv123@gmail.com>
2023-12-25 00:09:00 +0800

    Fix bugs in search for notes and archivedNotes

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -4,95 +4,89 @@ import NoteHeader from './NoteHeader';
 import NoteBody from './NoteBody';
 
 class NoteApp extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			notes: getInitialData(),
-			searchKeywords: '',
-			archivedNotes: [],
-		};
+    this.state = {
+      notes: getInitialData(),
+      searchKeywords: '',
+      archivedNotes: [],
+    };
 
-		this.onDeleteHandler = this.onDeleteHandler.bind(this);
-		this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
-		this.onArchiveChangeHandler = this.onArchiveChangeHandler.bind(this);
-		this.onSearchHandler = this.onSearchHandler.bind(this);
-		this.filteredNotes = this.filteredNotes.bind(this);
-	}
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
+    this.onArchiveChangeHandler = this.onArchiveChangeHandler.bind(this);
+    this.onSearchHandler = this.onSearchHandler.bind(this);
+    this.filteredNotes = this.filteredNotes.bind(this);
+  }
 
-	onDeleteHandler(id) {
-		const notes = this.state.notes.filter((note) => note.id !== id);
-		const archivedNotes = this.state.archivedNotes.filter(
-			(note) => note.id !== id
-		);
+  onDeleteHandler(id) {
+    const notes = this.state.notes.filter((note) => note.id !== id);
+    const archivedNotes = this.state.archivedNotes.filter((note) => note.id !== id);
 
-		this.setState({ notes, archivedNotes });
-	}
+    this.setState({ notes, archivedNotes });
+  }
 
-	onAddNoteHandler({ title, body, archived }) {
-		this.setState((prevState) => {
-			const date = new Date().toISOString();
-			return {
-				notes: [
-					...prevState.notes,
-					{
-						id: +new Date(),
-						title,
-						body,
-						createdAt: date,
-						archived,
-					},
-				],
-			};
-		});
-	}
+  onAddNoteHandler({ title, body, archived }) {
+    this.setState((prevState) => {
+      const date = new Date().toISOString();
+      return {
+        notes: [
+          ...prevState.notes,
+          {
+            id: +new Date(),
+            title,
+            body,
+            createdAt: date,
+            archived,
+          },
+        ],
+      };
+    });
+  }
 
-	onArchiveChangeHandler(id) {
-		const noteToArchive = this.state.notes.find((note) => note.id === id);
-		const archiveToNote = this.state.archivedNotes.find(
-			(note) => note.id === id
-		);
-		if (noteToArchive) {
-			this.setState((prevState) => ({
-				notes: prevState.notes.filter((note) => note.id !== id),
-				archivedNotes: [...prevState.archivedNotes, noteToArchive],
-			}));
-		} else {
-			this.setState((prevState) => ({
-				archivedNotes: prevState.archivedNotes.filter((note) => note.id !== id),
-				notes: [...prevState.notes, archiveToNote],
-			}));
-		}
-	}
+  onArchiveChangeHandler(id) {
+    const noteToArchive = this.state.notes.find((note) => note.id === id);
+    const archiveToNote = this.state.archivedNotes.find((note) => note.id === id);
+    if (noteToArchive) {
+      this.setState((prevState) => ({
+        notes: prevState.notes.filter((note) => note.id !== id),
+        archivedNotes: [...prevState.archivedNotes, noteToArchive],
+      }));
+    } else {
+      this.setState((prevState) => ({
+        archivedNotes: prevState.archivedNotes.filter((note) => note.id !== id),
+        notes: [...prevState.notes, archiveToNote],
+      }));
+    }
+  }
 
-	onSearchHandler(value) {
-		this.setState({ searchKeywords: value });
-	}
+  onSearchHandler(value) {
+    this.setState({ searchKeywords: value });
+  }
 
-	filteredNotes() {
-		const { notes, searchKeywords } = this.state;
-		return notes.filter((note) =>
-			note.title.toLowerCase().includes(searchKeywords.toLowerCase())
-		);
-	}
+  filteredNotes(notes, searchKeywords) {
+    const noted = notes.filter((note) => {
+      return note.title.toLowerCase().includes(searchKeywords.toLowerCase());
+    });
+    return noted;
+  }
 
-	render() {
-		return (
-			<>
-				<NoteHeader
-					value={this.state.searchKeywords}
-					onSearch={this.onSearchHandler}
-				/>
-				<NoteBody
-					notes={this.filteredNotes()}
-					archivedNotes={this.state.archivedNotes}
-					addNote={this.onAddNoteHandler}
-					onDelete={this.onDeleteHandler}
-					onArchive={this.onArchiveChangeHandler}
-				/>
-			</>
-		);
-	}
+  render() {
+    const { notes, archivedNotes, searchKeywords } = this.state;
+    return (
+      <>
+        <NoteHeader value={this.state.searchKeywords} onSearch={this.onSearchHandler} />
+        <NoteBody
+          notes={this.filteredNotes(notes, searchKeywords)}
+          archivedNotes={this.filteredNotes(archivedNotes, searchKeywords)}
+          addNote={this.onAddNoteHandler}
+          onDelete={this.onDeleteHandler}
+          onArchive={this.onArchiveChangeHandler}
+        />
+      </>
+    );
+  }
 }
 
 export default NoteApp;
